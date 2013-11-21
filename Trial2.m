8ad40bef5f8b958398d7376d60b9c58f7d17cf21
ShilpaAppannavar <shilpa.appannavar@gmail.com>
2013-11-21 15:40:12 +0100

    Main program for calibrating with simulated data.

---
+++ Trial2.m
@@ -0,0 +1,27 @@
+disp('Select the type of curve to be Simulated for the Calibration')
+disp('1= straight line, 2=sinusoidal curve, 3=spline')
+choice= input('Enter the choice:') %Enter the choice of cureve to be simulated for calibrating the sensors
+[xt yt]= curve(choice) % The x ad y co-ordinates give the tag positions as a mathematical function
+
+Sm=[0,0];           %Co-cordinates of a Master sensor.
+ns=input('enter the number of slave sensors')% Number of slave sensors to be calibrated
+S(1,:)=[0,20];      %Co-ordinate of Slave Sensor 1
+% S(2,:)=[,];         %Co-ordinate of Slave Sensor 2
+% S(3,:)=[,];         %Co-ordinate of Slave Sensor 3
+
+% m=number of target positions (get it by determining the size of xt)
+
+v=3*e8; %speed of the waves
+
+Sc(1,:)=[,]      %Co-ordinate of calibrated Slave Sensor 1
+% Sc(2,:)=[,]         %Co-ordinate of calibrated  Slave Sensor 2
+% Sc(3,:)=[,]         %Co-ordinate calibrated  Slave Sensor 3
+
+
+
+%%%% Call the function to generate the simulated TDOA measurements
+
+td=[];%time difference form each of the target positions
+
+%%%% Call the function to calibrate the sensor nodes.
+
