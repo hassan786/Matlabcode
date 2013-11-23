68a16d5d0f13ef41d35f65407572450c6fa35d34
ShilpaAppannavar <shilpa.appannavar@gmail.com>
2013-11-23 10:32:26 +0100

    Added the "SCalibrate" function call

--- Trial2.m
+++ Trial2.m
@@ -1,16 +1,26 @@
-disp('Select the type of curve to be Simulated for the Calibration')
+%Program to calibrate the sensor nodes using the simulated data.
+disp('Select the type of curve to be Simulated for the Calibration') 
 disp('1= straight line, 2=sinusoidal curve, 3=spline')
-choice= input('Enter the choice:') %Enter the choice of cureve to be simulated for calibrating the sensors
-[xt yt]= curve(choice); % The x ad y co-ordinates give the tag positions as a mathematical function
+choice= input('Enter the choice:')  %Enter the choice of cureve to be simulated for calibrating the sensors
 
-Sm=[0,0];           %Co-cordinates of a Master sensor.
+[xt,yt]= curve(choice);             % The x and y co-ordinates give the tag positions as a mathematical function
+
+Sm=[0,0];                           %Co-cordinates of a Master sensor.
 ns=input('enter the number of slave sensors')% Number of slave sensors to be calibrated
-S(1,:)=[0,20];      %Co-ordinate of Slave Sensor 1
-% S(2,:)=[,];         %Co-ordinate of Slave Sensor 2
-% S(3,:)=[,];         %Co-ordinate of Slave Sensor 3
-v=3e8; %speed of the waves
+S(1,:)=[0,20];          %Co-ordinate of Slave Sensor 1
+% S(2,:)=[,];           %Co-ordinate of Slave Sensor 2
+% S(3,:)=[,];           %Co-ordinate of Slave Sensor 3
+
+v=3e8;                  %speed of the waves
 
 % m=number of target positions (get it by determining the size of xt)
+[m,n] = size(xt);
+
+%Calculating r0 (the distance of each of the target position from the master sensor)
+for i=1:m
+    r0=(xt-Sm(1,1)).^2+(yt-Sm(1,2).^2);
+end
+
 %%%% Call the function to generate the simulated TDOA measurements
 
 for i=1:ns
@@ -20,13 +30,15 @@ for i=1:ns
 end
 
 
-Sc(1,:)=[,]      %Co-ordinate of calibrated Slave Sensor 1
-% Sc(2,:)=[,]         %Co-ordinate of calibrated  Slave Sensor 2
-% Sc(3,:)=[,]         %Co-ordinate calibrated  Slave Sensor 3
+Sc(1,:)=[,]         %Co-ordinate of calibrated Slave Sensor 1
+% Sc(2,:)=[,]       %Co-ordinate of calibrated  Slave Sensor 2
+% Sc(3,:)=[,]       %Co-ordinate calibrated  Slave Sensor 3
+
+%%%% Call the function "SCalibrate" to calibrate the sensor nodes.
+for i=1:ns
+    Sc(i,:)= SCalibrate(r0,xt,yt,v,td);         %Calibrating each of the sensor nodes
+end
 
 
 
-
-
-%%%% Call the function to calibrate the sensor nodes.
 
