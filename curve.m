f933fe9b8b856d86ff9edc113b666ab36bb9e0c9
hassan786 <hasanairian@gmail.com>
2013-11-23 14:45:56 +0100

    .

--- curve.m
+++ curve.m
@@ -1,11 +1,3 @@
-x = -pi:.1:pi;
-y = sin(x);
-plot(x,y)
-set(gca,'XTick',-pi:pi/2:pi)
-set(gca,'XTickLabel',{'-pi','-pi/2','0','pi/2','pi'})
-title('Sine Function');
-xlabel('Radians');
-ylabel('Function Value');
 function [m n]= curve(t)                        %this function generates a simulated data based on the selection
 switch t                                        %switch Switch among several cases 
     
