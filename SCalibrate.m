8aab8e6b9cb2694368f254bf327459fd39139fa1
ShilpaAppannavar <shilpa.appannavar@gmail.com>
2013-12-17 15:26:40 +0100

    minor changes
    absolute value not calculated

--- SCalibrate.m
+++ SCalibrate.m
@@ -5,4 +5,4 @@
 function objfunc= SCalibrate(Sc1,r0,xt,yt,td,v)
 d0=r0; %sqrt((xt-Sm(1,1)).^2+(yt-Sm(1,2)).^2);
 d1= sqrt((xt-Sc1(1)).^2+(yt-Sc1(2)).^2);
-objfunc= abs(d0-d1)-v*td;
+objfunc= (d0-d1)-v*td;
