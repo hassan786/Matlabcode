ce389cd7b8f78831dc2144657d8e5173c2a851d3
mvkmth <mvkmth96@gmail.com>
2013-12-18 11:18:08 +0100

    converted to matrix form
    still not completely generalized.

--- Program Optimization/Calibrate1.m
+++ Program Optimization/Calibrate1.m
@@ -7,16 +7,19 @@ choice= input('Enter the choice:')  %Enter the choice of cureve to be simulated 
 [xt,yt]= curve(choice);             % The x and y co-ordinates give the tag positions as a mathematical function
 
 Sm=[0,0];          % Co-ordinates of master sensor
-S1=[0,6];          %Co-ordinate of Slave Sensor 1
-S2=[7,6]; 
-S3=[7,0];
+S(1,:)=[0,6];          %Co-ordinate of Slave Sensor 1
+S(2,:)=[7,6]; 
+S(3,:)=[7,0];
 v=3e8;             %speed of the waves in m/s
 
 r0=sqrt((xt-Sm(1)).^2+(yt-Sm(2)).^2);
-td1= timediff(r0,xt,yt,S1,v); %time difference form each of the target positions for sensor 1 to master
-td2= timediff(r0,xt,yt,S2,v); %time difference form each of the target positions for sensor 1 to master
-td3= timediff(r0,xt,yt,S3,v); %time difference form each of the target positions for sensor 1 to master
+for i=1:3
+    Sl=S(i,:);
+    td(i,:)= timediff(r0,xt,yt,Sl,v); %time difference form each of the target positions for sensor 1 to master
+%td2= timediff(r0,xt,yt,S2,v); %time difference form each of the target positions for sensor 1 to master
+%td3= timediff(r0,xt,yt,S3,v); %time difference form each of the target positions for sensor 1 to master
+end
 
-Sc=lsqnonlin(@(Sc)SCalibrate1(Sc,r0,xt,yt,td1,td2,td3,v),([0,0,0,0,0,0])); 
+Sc=lsqnonlin(@(Sc)SCalibrate1(Sc,r0,xt,yt,td,v),([0,0;0,0;0,0])) 
     % least square non-linear optamization function call
     % The initial guess of 1,0 for the third sensor gives accurate results
--- Program Optimization/SCalibrate1.m
+++ Program Optimization/SCalibrate1.m
@@ -1,10 +1,10 @@
-function ErrFun= SCalibrate1(Sc,r0,xt,yt,td1,td2,td3,v)
+function ErrFun= SCalibrate1(Sc,r0,xt,yt,td,v)
 
-       r1=sqrt((xt-Sc(1)).^2+(yt-Sc(2)).^2);
-       e1= r0-r1-td1*v;
-       r2=sqrt((xt-Sc(3)).^2+(yt-Sc(4)).^2);
-       e2= r0-r2-td2*v;
-       r3=sqrt((xt-Sc(5)).^2+(yt-Sc(6)).^2);
-       e3= r0-r3-td3*v;
-       ErrFun=[e1,e2,e3];
+       r1=sqrt((xt-Sc(1,1)).^2+(yt-Sc(1,2)).^2);
+       e1= r0-r1-td(1,:)*v;
+       r2=sqrt((xt-Sc(2,1)).^2+(yt-Sc(2,2)).^2);
+       e2= r0-r2-td(2,:)*v;
+       r3=sqrt((xt-Sc(3,1)).^2+(yt-Sc(3,2)).^2);
+       e3= r0-r3-td(3,:)*v;
+       ErrFun=[e1;e2;e3];
 end
