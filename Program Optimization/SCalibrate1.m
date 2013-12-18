1ed40bc6ae75745bb633d649b8725463d87cde46
ShilpaAppannavar <shilpa.appannavar@gmail.com>
2013-12-18 21:02:42 +0100

    reducing the number of lines of the code
    the entire calculation is done using a for loop.

--- Program Optimization/SCalibrate1.m
+++ Program Optimization/SCalibrate1.m
@@ -1,10 +1,8 @@
 function ErrFun= SCalibrate1(Sc,r0,xt,yt,td,v)
 
-       r1=sqrt((xt-Sc(1,1)).^2+(yt-Sc(1,2)).^2);
-       e1= r0-r1-td(1,:)*v;
-       r2=sqrt((xt-Sc(2,1)).^2+(yt-Sc(2,2)).^2);
-       e2= r0-r2-td(2,:)*v;
-       r3=sqrt((xt-Sc(3,1)).^2+(yt-Sc(3,2)).^2);
-       e3= r0-r3-td(3,:)*v;
-       ErrFun=[e1;e2;e3];
+  for p=1:3
+     r(p,:)=sqrt((xt-Sc(p,1)).^2+(yt-Sc(p,2)).^2);
+     ErrFun(p,:)=r0-r(p,:)-td(p,:)*v;
+  end
+
 end
