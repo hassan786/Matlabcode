bf908ba83016374c9467244f371a05967edee713
hassan786 <hasanairian@gmail.com>
2014-01-07 13:06:02 +0100

    added noise in 3D

--- TDOA 3D/timediff_3D.m
+++ TDOA 3D/timediff_3D.m
@@ -6,6 +6,12 @@ function y= timediff_3D(r0,xt,yt,zt,Sl,v)
 %function calculating distence vector 'r'  for each of the slave sensor
 r = sqrt((xt-Sl(1)).^2+(yt-Sl(2)).^2+(zt-Sl(3)).^2); % get real distance from emitter for each and one of the stations
 y=(r0-r)/v;
+ scale=0.1;% add  noise in term of percentage based on gaussian 
+    
+[m,n] = size(xt);
+%f2=rd/v;
+%f= rd/v + f1.*(rd/v)*scale;
+y=((r0-r)/v)+((r0-r)/v).*normrnd(0,1,[1,n])*scale;
 end
 
     
